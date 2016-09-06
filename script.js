4199463cf5cdb90046483206313cc20bdd5ea97d
Andrey13 <ademidenko87@mail.ru>
2016-09-06 12:34:07 +0300

    JS_HW_1

---
+++ script.js
@@ -0,0 +1,9 @@
+/**
+ * Created by ademidenko on 06.09.2016.
+ */
+var a= prompt('¬ведите любое число','');
+var b= prompt('¬ведите степень числа','');
+
+var result= Math.pow(a,b);
+
+console.log(result);
